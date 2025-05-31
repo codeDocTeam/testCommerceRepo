345b3f2582a64910d019fa0636953a130b1509fa
manavbhagwani <64921323+manavbhagwani@users.noreply.github.com>
2025-05-31 21:48:55 +0530

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -19,12 +19,12 @@ export default async function handler(req, res) {
 
           return {
             price_data: { 
-              currency: 'gbp',
+              currency: 'usd',
               product_data: { 
                 name: item.name,
                 images: [newImage],
               },
-              unit_amount: item.price * 100,
+              unit_amount: item.price * 10,
             },
             adjustable_quantity: {
               enabled:true,
